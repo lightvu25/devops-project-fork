32d583b299c554a315fa3b4e9d276e616232b560
QuangZu <minhquangvuxd@gmail.com>
2026-02-25 12:07:26 +0700

    Update CI/CD pipeline, start.bat, stop.bat

---
+++ start.bat
@@ -0,0 +1,17 @@
+@echo off
+
+echo.
+echo 1. Starting SSH service for CI/CD...
+wsl -d Ubuntu -u root service ssh start
+
+echo.
+echo 2. Pulling latest code...
+wsl -d Ubuntu -u quang -e bash -c "cd ~/devops-project-fork && git pull origin main"
+
+echo.
+echo 3. Starting Todo (Frontend + Backend + DB)...
+wsl -d Ubuntu -u quang -e bash -c "cd ~/devops-project-fork && docker compose pull && docker compose up -d"
+
+echo.
+echo SERVER ONLINE
+pause
