d0d969ce0d60d7fb396e6f0c2f8acc027374ee5a
QuangZu <minhquangvuxd@gmail.com>
2026-02-27 10:07:54 +0700

    fixed wsl dir and name tunnel

--- start.bat
+++ start.bat
@@ -6,11 +6,11 @@ wsl -d Ubuntu -u root service ssh start
 
 echo.
 echo 2. Pulling latest code...
-wsl -d Ubuntu -u quang -e bash -c "cd ~/devops-project-fork && git pull origin main"
+wsl -d Ubuntu -u quang -e bash -c "cd /mnt/d/Devops/devops-project-fork && git pull origin main"
 
 echo.
 echo 3. Starting Todo (Frontend + Backend + DB)...
-wsl -d Ubuntu -u quang -e bash -c "cd ~/devops-project-fork && docker compose pull && docker compose up -d --build"
+wsl -d Ubuntu -u quang -e bash -c "cd /mnt/d/Devops/devops-project-fork && docker compose pull && docker compose up -d --build"
 
 echo.
 echo SERVER ONLINE
